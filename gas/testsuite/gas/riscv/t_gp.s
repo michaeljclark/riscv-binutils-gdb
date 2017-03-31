094d9a6f7e070f40b2264e125e19cff018c4e5ef
Palmer Dabbelt <palmer@dabbelt.com>
2017-03-31 15:46:38 -0700

    GP disassembly test

---
+++ gas/testsuite/gas/riscv/t_gp.s
@@ -0,0 +1,8 @@
+.data
+__global_pointer$:
+global_symol:
+	.word 
+
+.text
+target:
+	ld s0, 0(gp)
---
+++ gas/testsuite/gas/riscv/t_gp_disas.d
@@ -0,0 +1,10 @@
+#as:
+#objdump: -dr
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+000 <target>:
+[ 	]+0:[ 	]+00000013[ 	]+ld s0, 0(gp) # global_symbol
